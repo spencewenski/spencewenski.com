e651eb7f741861a098fe7518d3ba51e33148364e
Spencer Ferris <3319370+spencewenski@users.noreply.github.com>
2023-02-28 11:32:38 -0800

    Add `health-check` api

---
+++ src/pages/api/health-check.ts
@@ -0,0 +1,12 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+type Output = {
+  message: string;
+};
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Output>
+) {
+  res.status(200).json({ message: "success" });
+}
