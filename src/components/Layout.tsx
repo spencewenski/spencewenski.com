1a492e22db0897178bc7eeb359152ec0ea74967c
Spencer Ferris <3319370+spencewenski@users.noreply.github.com>
2023-02-28 21:55:15 -0800

    Add margin to bottom of page

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,10 +1,10 @@
-import { CssBaseline } from "@mui/material";
+import { Box, CssBaseline } from "@mui/material";
 
 export default function Layout({ children }: any) {
   return (
-    <>
+    <Box marginBottom={"1em"}>
       <CssBaseline />
       {children}
-    </>
+    </Box>
   );
 }
