39ebef0e65938c030b66c2f0f8c5dd60c55cf100
Spencer Ferris <3319370+spencewenski@users.noreply.github.com>
2023-03-01 00:57:51 -0800

    Update Amazon summaries and mass dissent preview

--- src/components/AmazonExperience.tsx
+++ src/components/AmazonExperience.tsx
@@ -8,20 +8,20 @@ const WSIW_PROJECT_INFO: ProjectInfo = {
   title: '"Alexa, What Should I Watch?" (Current Project)',
   imageUrl: "/wsiw_preview.jpeg",
   summary: [
-    '"What Should I Watch?" is a conversational Alexa skill for FireTV that recommends content to watch. I work closely with PMs/TPMs to align on the desired features, with UX on how to best design the features, and with the Alexa Conversations team to implement our dialog model and improve the Alexa Conversations platform.',
-    "The backend skill code is written using Java/Spring and is deployed on EC2, the UI is written using Alexa APL, and the dialog model is written using ACDL",
+    '"What Should I Watch?" (WSIW) is a conversational Alexa skill for FireTV that recommends content to watch. I work closely with PMs/TPMs to align on the desired features, with UX on how to best design the features, and with the Alexa Conversations team to implement our dialog model and improve the Alexa Conversations platform.',
+    "The backend skill code is written using Java/Spring and is deployed on EC2, the UI is written using Alexa APL, and the dialog model is written using ACDL.",
   ],
   technologies: ["Java", "Spring", "Alexa", "AWS"],
 };
 
 const SDE_II_NOTABLE_ACHIEVEMENTS: Array<string> = [
-  "Designed and implemented an Android application to enable Amazon Music and other media apps to integrate with Alexa for fully featured voice support",
-  "Collaborated with the Alexa Video team to enable playing a specific season and episode of a show using voice",
-  "Lead project to ensure operational readiness of multiple services ahead of expected holiday traffic spikes",
-  "Investigated numerous issues involving multiple services owned by various different teams",
-  "Performed code reviews regularly to maintain code quality, share knowledge, and mentor team members",
-  "Mentored interns and new hires to help them become confident and productive members of the team",
-  "Interviewed candidates at my level and below across multiple technical and leadership competencies",
+  "Managed multiple beta releases before the public release of WSIW.",
+  "Collaborated with an Alexa Science team to improve the level of personalization of our recommendations.",
+  "Ran A/B tests to measure how new features and UX changes impact our key metrics.",
+  "Developed an Android application to enable Amazon Music and other media apps to integrate with Alexa for fully featured voice support.",
+  "Worked with the Alexa Video team to enable playing a specific season and episode of a show using Alexa.",
+  "Ran a working group to improve the operational excellence of my team's (and our sister team's) code.",
+  "Actively involved in mentoring, code reviews, and interviewing.",
 ];
 
 const SDE_I_NOTABLE_ACHIEVEMENTS: Array<string> = [
