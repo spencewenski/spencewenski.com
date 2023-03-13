db4c8e57f28c42b059cc836da191d617beca7673
Spencer Ferris <3319370+spencewenski@users.noreply.github.com>
2023-03-13 01:10:51 -0700

    Register tRPC health check with router

--- src/server-trpc/routers/public/_public.ts
+++ src/server-trpc/routers/public/_public.ts
@@ -1,6 +1,8 @@
 import { router } from "@/server-trpc/trpc";
 import { sendMessageFromContactPage } from "@/server-trpc/routers/public/sendMessageFromContactPage";
+import { healthCheck } from "@/server-trpc/routers/public/healthCheck";
 
 export const publicAppRouter = router({
   sendMessageFromContactPage,
+  healthCheck,
 });
