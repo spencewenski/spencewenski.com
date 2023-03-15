9e1f6b0e5fa6932357affa9703492ae2c236107d
Spencer Ferris <3319370+spencewenski@users.noreply.github.com>
2023-03-15 02:08:17 -0700

    Add some security headers

--- next.config.js
+++ next.config.js
@@ -1,6 +1,33 @@
+// Todo: add a `Content-Security-Policy`
+const SECURITY_HEADERS = [
+  {
+    key: "Strict-Transport-Security",
+    value: "max-age=63072000; includeSubDomains; preload",
+  },
+  {
+    key: "X-XSS-Protection",
+    value: "1; mode=block",
+  },
+  {
+    key: "Referrer-Policy",
+    value: "no-referrer",
+  },
+];
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
+  async headers() {
+    return [
+      {
+        // https://nextjs.org/docs/advanced-features/security-headers
+        source: "/:path*",
+        headers: SECURITY_HEADERS,
+      },
+    ];
+  },
+
   // https://nextjs.org/docs/api-reference/next.config.js/custom-webpack-config
   webpack: (config, options) => {
     config.module.rules.push({
