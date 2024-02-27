297591f998a30f54c23166214bcbe9aa448811bc
Spencer Ferris <3319370+spencewenski@users.noreply.github.com>
2024-02-27 15:35:14 -0800

    Add some content to about and now pages

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -5,7 +5,11 @@ import SectionWrapper from "@/components/SectionWrapper";
 import BulletedList from "@/components/BulletedList";
 
 const UPDATED: string = "27 Feb 2024";
-const DONE_PREVIOUSLY: Array<string> = [];
+const DONE_PREVIOUSLY: Array<string> = [
+  "I have run a couple half marathons and would love to run a full marathon in the next couple years.",
+  "I love playing video games and watching movies and TV.",
+  "I love playing around with new (to me) technologies. A roughly chronological list of things I've played around with include C/C++, Unity/C#, Pebble, Godot, Raspberry Pi, Rust, React (NextJS), and Tauri.",
+];
 
 export default function About() {
   return (
@@ -17,8 +21,9 @@ export default function About() {
       <>
         <Typography variant={"h3"}>About</Typography>
         <Typography variant={"subtitle1"}>
-          A brief summary of what I&apos;ve worked on in the past. See my{" "}
-          <Link href={"/"}>portfolio page</Link> for more details.
+          A brief summary of what I&apos;ve worked on in the past, mostly on a
+          personal level. See my <Link href={"/"}>portfolio page</Link> for more
+          details.
         </Typography>
         <Typography variant={"subtitle2"}>Last updated: {UPDATED}</Typography>
         <SectionWrapper>
--- src/pages/now.tsx
+++ src/pages/now.tsx
@@ -4,7 +4,9 @@ import SectionWrapper from "@/components/SectionWrapper";
 import BulletedList from "@/components/BulletedList";
 
 const UPDATED: string = "27 Feb 2024";
-const DOING_NOW: Array<string> = [];
+const DOING_NOW: Array<string> = [
+  "I'm playing around with a using Rust for a REST API service. Currently just trying to bootstrap a starter service with some essentials (auth/tracing/testing/etc).",
+];
 
 export default function Now() {
   return (
