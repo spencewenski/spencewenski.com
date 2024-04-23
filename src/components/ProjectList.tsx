efbc62b34a55d6fbe770ec52a7ab0ac97e47bdf2
Spencer Ferris <3319370+spencewenski@users.noreply.github.com>
2024-04-22 17:38:02 -0700

    Add roadster to projects list

--- src/components/ProjectList.tsx
+++ src/components/ProjectList.tsx
@@ -1,10 +1,68 @@
 import ProjectListItem from "@/components/ProjectListItem";
-import { Grid } from "@mui/material";
+import { Grid, Typography } from "@mui/material";
 import { ProjectInfo } from "@/components/ProjectDetails";
 import MassDissentPreview from "@/images/mass_dissent_preview.webp";
 import GroupedTimersPreview from "@/images/grouped_timers_preview.png";
+import Link from "next/link";
 
 const PROJECTS: Array<ProjectInfo> = [
+  {
+    title: "Roadster",
+    summary: [
+      <>
+        A &quot;Batteries Included&quot; web framework for rust designed to get
+        you moving fast 🏎️. Inspired by other fully-featured frameworks such as{" "}
+        <Link href={"https://rubyonrails.org/"}>Rails</Link>,{" "}
+        <Link href={"https://www.djangoproject.com/"}>Django</Link>,{" "}
+        <Link href={"https://laravel.com/"}>Laravel</Link>,{" "}
+        <Link href={"https://github.com/loco-rs/loco"}>Loco</Link>, and{" "}
+        <Link href={"https://github.com/poem-web/poem"}>Poem</Link>.
+      </>,
+      // eslint-disable-next-line react/jsx-key
+      <ul>
+        <li>
+          Built on <Link href={"https://tokio.rs/"}>Tokio's</Link> web stack
+          (axum, tower, hyper, tracing). App behavior can be easily extended by
+          taking advantage of all the resources in the tokio ecosystem.
+        </li>
+        <li>
+          Provides sane defaults so you can focus on building your app. Most of
+          the built-in behavior can be customized or even disabled via
+          per-environment configuration files.
+        </li>
+        <li>
+          Auto-generates an OpenAPI schema for routes defined with{" "}
+          <Link href={"https://crates.io/crates/aide"}>aide</Link>.
+        </li>
+        <li>
+          Built-in support for{" "}
+          <Link href={"https://crates.io/crates/sea-orm"}>SeaORM</Link>,
+          including creating DB connections and running DB migrations.
+        </li>
+        <li>
+          Built-in support for{" "}
+          <Link href={"https://crates.io/crates/rusty-sidekiq"}>
+            Sidekiq.rs
+          </Link>{" "}
+          for running async/background jobs.
+        </li>
+        <li>
+          Structured logs/traces using Tokio's{" "}
+          <Link href={"https://docs.rs/tracing/latest/tracing/"}>tracing</Link>{" "}
+          crate. Traces/metrics can be exported using OpenTelemetry.
+        </li>
+      </ul>,
+    ],
+    technologies: [
+      "Rust",
+      "Sidekiq",
+      "Postgres",
+      "SeaORM",
+      "OpenAPI",
+      "OpenTelemetry",
+    ],
+    source: "https://github.com/roadster-rs/roadster",
+  },
   {
     title: "Mass Dissent",
     imageUrl: MassDissentPreview,
