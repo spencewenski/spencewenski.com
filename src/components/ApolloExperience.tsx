7a06891f44fe023ddaa624fb2423ebc1fd2596fb
Spencer Ferris <3319370+spencewenski@users.noreply.github.com>
2025-08-08 16:12:55 -0700

    Update apollo experience

--- src/components/ApolloExperience.tsx
+++ src/components/ApolloExperience.tsx
@@ -5,12 +5,15 @@ import BulletedList from "@/components/BulletedList";
 import TechnologiesList from "@/components/TechnologiesList";
 
 const APOLLO_SENIOR_ENGINEER: Array<string> = [
-  "Senior engineer and tech lead on the Conversation Intelligence team",
-  "Lead adoption of the Sorbet type system for Apollo's Ruby on Rails monolith",
-  "Increased conversation processing workflow success rate from less than 70% to over 95%",
-  "Created framework to generate an OpenAPI schema from Sorbet-annotated Rails controller methods",
-  "Partners with PM, Research, and Design to develop CX improvements and drive adoption of our features",
-  "Efficiently builds new features using Rails and reusable React components, complete with automated tests",
+    "Senior engineer and tech lead on the Conversation Intelligence team",
+    "Improved stability of the Pre-Meeting Insights feature by reducing latency by 10x and error rate by 7x",
+    "Increased conversation processing workflow success rate from less than 70% to over 95%",
+    "Lead adoption of the Sorbet type system for Apollo's Ruby on Rails monolith",
+    "Created framework to generate an OpenAPI schema from Sorbet-annotated Rails controller methods",
+    "Partners with PM, Research, and Design to develop CX improvements and drive adoption of our features",
+    "Defines high-level architecture requirements and reviews the team's engineering designs",
+    "Efficiently builds new features using Rails and reusable React components, complete with automated tests",
+    "Performs interviews and regularly meets with team members for mentorship and team health check-ins",
 ];
 
 const TECHNOLOGIES: Array<string> = [
