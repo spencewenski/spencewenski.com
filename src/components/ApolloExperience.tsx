3af33cbb264164c120d083c830f7d7b971fcf1c6
Spencer Ferris <3319370+spencewenski@users.noreply.github.com>
2026-07-02 17:02:44 -0700

    Update Apollo experience

--- src/components/ApolloExperience.tsx
+++ src/components/ApolloExperience.tsx
@@ -5,19 +5,18 @@ import BulletedList from "@/components/BulletedList";
 import TechnologiesList from "@/components/TechnologiesList";
 
 const APOLLO_SENIOR_ENGINEER: Array<string> = [
+  "Created workflow trigger to automate followups when prospects do not attend sales calls; closed 1M USD gap in ARR",
   "Improved latency of the Coaching Analytics module by 10x using Elasticsearch aggregations",
-  "Created workflow trigger critical in closing 300K - 1M gap in ARR",
   "Added integration between Conversations and Tasks to improve WAU metric",
+  "Lead company-wide initiative to adopt Sorbet type system; increased coverage from 7% to 35% in one year, resulting in significantly fewer bugs and higher developer velocity due to improved IDE type inference",
+  "Collaborated across teams to improve stability of Pre-Meeting Insights; reduced latency by 7x and error rate by 10x",
   "Reduced cost of conversation recording storage by 100K USD / year",
   "Increased conversation processing workflow success rate from less than 70% to over 95%",
-  "Improved stability of the Pre-Meeting Insights feature by reducing latency by 7x and error rate by 10x",
   "Prevented privacy/security issues by creating access policies for our DB entities",
-  "Lead adoption of Sorbet; increased coverage from 7% to 35% in one year for a 60% reduction in production type errors",
   "Created framework to generate an OpenAPI schema from Sorbet-annotated Rails controller methods",
-  "Defines high-level architecture requirements and reviews the team's engineering designs",
-  "Efficiently builds new features using Rails and reusable React components, complete with automated tests",
-  "Performs interviews and regularly meets with team members for mentorship and team health check-ins",
   "Heavily involved in code reviews to spread knowledge, encourage best practices, and catch performance/security issues",
+  "Defines high-level system architecture and reviews the team's engineering designs",
+  "Performs technical interviews and mentors team members to achieve IC-track promotions",
 ];
 
 const TECHNOLOGIES: Array<string> = [
